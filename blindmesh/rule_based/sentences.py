ea27b4b4fd0959ee954a293a015d6d9d13be054a
Márton Kardos <power.up1163@gmail.com>
2023-07-25 16:00:20 +0200

    Added docs to SentenceFilter

--- blindmesh/rule_based/sentences.py
+++ blindmesh/rule_based/sentences.py
@@ -1,5 +1,4 @@
 import random
-from collections import Counter
 from typing import Union
 
 import numpy as np
@@ -7,6 +6,25 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class SentenceFilter(TransformerMixin, BaseEstimator):
+    """Sklearn component that filters out certain sentences given some attributes
+    of them. Texts that are passed to the component have to be sentencized and
+    tokenized already.
+    All parameters are optional, if nothing is passed into the constructor
+    it will act as the identity.
+
+    Parameters
+    ----------
+    length_range: tuple, default (0, None)
+        A tuple of the lower and upper boundary on sentence length.
+        If an integer it will be interpreted as absolute character count.
+        If a float it will be interpreted as a quantile, and will be learned
+        from data using reservoir sampling.
+    max_memory: int, default 5000
+        Maximum size of the reservoir sampled sentence length pool.
+        This is what the lenght quantiles are calculated from.
+        Lower number means worse estimate, but lower memory usage.
+    """
+
     def __init__(
         self,
         length_range: tuple[Union[int, float], Union[int, float, None]] = (
@@ -21,15 +39,15 @@ class SentenceFilter(TransformerMixin, BaseEstimator):
             self.min_length_quantile = False
             self.min_length = self.minl
         elif isinstance(self.minl, float):
-            self.min_length_quantile = False
+            self.min_length_quantile = True
             self.min_length = 0
         else:
             raise ValueError("Minimum length either has to be float or int.")
         if isinstance(self.maxl, int):
             self.max_length_quantile = False
-            self.max_length = self.minl
+            self.max_length = self.maxl
         elif isinstance(self.maxl, float):
-            self.max_length_quantile = False
+            self.max_length_quantile = True
             self.max_length = None
         elif self.maxl is None:
             self.max_length_quantile = False
@@ -41,6 +59,7 @@ class SentenceFilter(TransformerMixin, BaseEstimator):
         self.max_memory = max_memory
 
     def append_reservoir(self, sentence: list[str]):
+        """Adds token length to the pool with reservoir sampling."""
         if self.seen_lengths < self.max_memory:
             self.length_pool.append(len(sentence))
         else:
@@ -50,9 +69,31 @@ class SentenceFilter(TransformerMixin, BaseEstimator):
         self.seen_lengths += 1
 
     def fit(self, X: list[list[list[str]]], y=None):
+        """Fits filter to the data.
+
+        Parameters
+        ----------
+        X: list of list of list of str
+            Documents sentencized and tokenized.
+
+        Returns
+        -------
+        self
+        """
         return self.partial_fit(X)
 
     def partial_fit(self, X: list[list[list[str]]], y=None):
+        """Fits filter to the data.
+
+        Parameters
+        ----------
+        X: list of list of list of str
+            Documents sentencized and tokenized.
+
+        Returns
+        -------
+        self
+        """
         for doc in X:
             for sent in doc:
                 self.append_reservoir(sent)
@@ -63,6 +104,7 @@ class SentenceFilter(TransformerMixin, BaseEstimator):
         return self
 
     def passes(self, sentence: list[str]) -> bool:
+        """Determines whether a sentence passes the filter or not."""
         sent_len = len(sentence)
         max_pass: bool = (self.max_length is None) or (
             sent_len <= self.max_length
@@ -71,11 +113,21 @@ class SentenceFilter(TransformerMixin, BaseEstimator):
         return max_pass and min_pass
 
     def transform(self, X: list[list[list[str]]]) -> list[list[list[str]]]:
+        """Filters out sentences based on the parameters of
+        the component.
+
+        Parameters
+        ----------
+        X: list of list of list of str
+            Documents sentencized and tokenized.
+
+        Returns
+        -------
+        list of list of list of str
+            Documents with sentences that pass the filter.
+        """
         res = []
         for doc in X:
-            res_sents = []
-            for sent in doc:
-                res_tokens = [token for token in sent if self.passes(token)]
-                res_sents.append(res_tokens)
+            res_sents = [sent for sent in doc if self.passes(sent)]
             res.append(res_sents)
         return res
